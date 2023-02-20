edb45a4fa22df78363442570100861dbad20a5d6
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2023-02-20 05:51:38 -0500

    I added the unit testing class

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -1,5 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class HangmanTest {
+    public Hangman obj;
 
-    public class HangmanTest {
-
+    @BeforeEach
+    public void setup() {
+        System.out.println("Instantiating object...");
+        obj = new Hangman();
     }
+
+    @Test
+    public void test1() {
+
+        // Act: execute the method you are testing
+        String actual = obj.welcome();
+
+        // Assert: check whether the actual result is equal to the expected result
+        String expected = "H A N G M A N";
+        Assertions.assertEquals(expected, actual); //Go and check for us
+    }
+}
